c0702cc41ef609e67f82c7c809dcb07a2282c8b3
Cristian Sfetcu <sfetcu.cristian93@gmail.com>
2022-10-31 23:55:25 +0200

    ah oy

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+function objToString(obj) {
+    let str = '';
+    for (const [p, val] of Object.entries(obj)) {
+        str += `${p}::${val}\n`;
+    }
+    return str;
+}
+
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(req.toString());
+    res.send(objToString(req));
 });
 
 app.listen(5000, () => {
