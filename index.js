807223d665d114809576cffa7fbd8da9b0041704
Cristian Sfetcu <sfetcu.cristian93@gmail.com>
2022-10-31 23:53:12 +0200

    ahhoyy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(JSON.stringify(req));
+    res.send(req.toString());
 });
 
 app.listen(5000, () => {
