0c710ad5c2be5d0512d68ad0c6fe951610b8b778
Cristian Sfetcu <sfetcu.cristian93@gmail.com>
2022-11-01 00:06:20 +0200

    i hate functions

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
 const express = require("express");
 
-function convertToText(obj) {
-    var string = [];
-    if (typeof (obj) == "object" && (obj.join == undefined)) {
-        string.push("{");
-        for (prop in obj) {
-            string.push(prop, ": ", convertToText(obj[prop]), ",");
-        };
-        string.push("}");
-
-    } else if (typeof (obj) == "object" && !(obj.join == undefined)) {
-        string.push("[")
-        for (prop in obj) {
-            string.push(convertToText(obj[prop]), ",");
-        }
-        string.push("]")
-
-        //is function
-    } else if (typeof (obj) == "function") {
-        string.push(obj.toString())
-
-        //all other values can be done with JSON.stringify
-    } else {
-        string.push(JSON.stringify(obj))
-    }
-
-    return string.join("")
-}
-
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(convertToText(req));
+    res.send(req);
 });
 
 app.listen(5000, () => {
